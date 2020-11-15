03557dd3d64a5f25d1bdb7c6c0c7f23b63e87917
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-11-15 18:44:54 +0000

    feat: error handler (#9)
    * :rocket: add AppError and error middleware
    
    * :rocket: add AppError to the use cases

--- packages/api/src/modules/todos/useCases/unfinishTask/unfinishTask.js
+++ packages/api/src/modules/todos/useCases/unfinishTask/unfinishTask.js
@@ -1,10 +1,10 @@
+const AppError = require('../../../../errors/AppError');
 const { taskRepo } = require('../../repos');
 
 module.exports = async ({ userId, taskId }) => {
   const task = await taskRepo.findByIdAndUser({ id: taskId, userId });
-  // TODO: throw an error
   if (!task) {
-    return null;
+    throw new AppError('Task not found', 404);
   }
 
   return task.update({ finishedAt: null });
--- packages/api/src/modules/todos/useCases/updateProject/updateProject.js
+++ packages/api/src/modules/todos/useCases/updateProject/updateProject.js
@@ -1,10 +1,10 @@
+const AppError = require('../../../../errors/AppError');
 const { projectRepo } = require('../../repos');
 
 module.exports = async ({ userId, projectId, name }) => {
   const project = await projectRepo.findByIdAndUser({ id: projectId, userId });
-  // TODO: throw an error
   if (!project) {
-    return null;
+    throw new AppError('Project not found', 404);
   }
 
   return project.update({ name });
