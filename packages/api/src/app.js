7f482a081264870b70a265dfd85d3e8040aec760
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-11-15 18:41:32 +0000

    :rocket: add AppError and error middleware

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 require('express-async-errors');
 
 require('./database');
+const AppError = require('./errors/AppError');
 const routes = require('./routes');
 
 const app = express();
@@ -11,5 +12,19 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      error: err.message,
+    });
+  }
+
+  console.error(err);
+  return res.status(500).json({
+    error: 'Internal server error',
+  });
+});
 
 module.exports = app;
