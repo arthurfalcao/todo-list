caef8f0a123200e67342497b5ac03212d48b2fcd
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-11-15 23:46:45 +0000

    :art: add styles to the login page

--- packages/web/src/pages/Login/Login.jsx
+++ packages/web/src/pages/Login/Login.jsx
@@ -7,7 +7,7 @@ import * as S from './styles';
 const rules = { required: true };
 
 function Login() {
-  const { register, handleSubmit, reset } = useForm({
+  const { register, handleSubmit, errors, reset } = useForm({
     defaultValues: {
       email: '',
       password: '',
@@ -26,12 +26,29 @@ function Login() {
 
   return (
     <S.Wrapper>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="email" type="email" ref={register(rules)} placeholder="Email" />
-        <input name="password" type="password" ref={register(rules)} placeholder="Senha" />
+      <S.ContentWrapper>
+        <S.Title>TODO List</S.Title>
 
-        <button type="submit">Login</button>
-      </form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <S.FormGroup>
+            <S.Input name="email" type="email" ref={register(rules)} placeholder="Email" error={!!errors.email} />
+          </S.FormGroup>
+
+          <S.FormGroup>
+            <S.Input
+              name="password"
+              type="password"
+              ref={register(rules)}
+              placeholder="Senha"
+              error={!!errors.password}
+            />
+          </S.FormGroup>
+
+          <S.Button fullWidth type="submit">
+            Login
+          </S.Button>
+        </form>
+      </S.ContentWrapper>
     </S.Wrapper>
   );
 }
--- packages/web/src/pages/Login/styles.js
+++ packages/web/src/pages/Login/styles.js
@@ -1,3 +1,66 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  color: #195b92;
+  font-size: 2.4rem;
+  text-align: center;
+`;
+
+export const ContentWrapper = styled.div`
+  max-width: 400px;
+  width: 100%;
+`;
+
+export const FormGroup = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  width: 100%;
+`;
+
+const inputModifiers = {
+  onError: () => css`
+    border-color: #e74c3c;
+
+    &:focus {
+      box-shadow: 0 0 0 1.5px #e74c3c;
+    }
+  `,
+};
+
+export const Input = styled.input`
+  ${({ error }) => css`
+    border: 3px solid #ffc12c;
+    border-radius: 0.5rem;
+    font-size: 1.8rem;
+    padding: 1.6rem 3.2rem;
+    width: 100%;
+
+    ${!!error && inputModifiers.onError()}
+  `}
+`;
+
+export const Button = styled.button`
+  ${({ fullWidth }) => css`
+    background: #ffc12c;
+    border: 0;
+    border-radius: 0.5rem;
+    color: #ffffff;
+    cursor: pointer;
+    font-size: 1.8rem;
+    font-weight: bold;
+    padding: 1.6rem 3.2rem;
+
+    ${fullWidth &&
+    css`
+      width: 100%;
+    `}
+  `}
+`;
