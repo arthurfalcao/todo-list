f57a20280be960c4025566c1f69026eea19407d5
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-11-16 05:07:58 +0000

    :rocket: add projects list to the home page

--- packages/web/src/pages/Home/Home.jsx
+++ packages/web/src/pages/Home/Home.jsx
@@ -1,19 +1,44 @@
-import CreateProjectForm from 'components/CreateProjectForm';
+import { useState, useEffect, useCallback } from 'react';
+
+import Container from 'components/Container';
 import Navbar from 'components/Navbar';
+import Project from 'components/Project';
+import ProjectForm from 'components/ProjectForm';
+
+import * as projectService from 'services/project';
 
 import * as S from './styles';
 
 function Home() {
+  const [projects, setProjects] = useState([]);
+
+  const fetchProjects = useCallback(async () => {
+    const { data } = await projectService.getAll();
+    setProjects(data);
+  }, []);
+
+  useEffect(() => {
+    fetchProjects();
+  }, [fetchProjects]);
+
+  const handleRemoveProject = (id) => {
+    setProjects((state) => state.filter((p) => p.id !== id));
+  };
+
   return (
-    <S.Wrapper>
+    <>
       <Navbar />
 
-      <S.Container>
+      <Container>
         <S.Content>
-          <CreateProjectForm />
+          {projects.map((project) => (
+            <Project key={project.id} {...project} onRemove={handleRemoveProject} />
+          ))}
+
+          <ProjectForm onAdded={fetchProjects} />
         </S.Content>
-      </S.Container>
-    </S.Wrapper>
+      </Container>
+    </>
   );
 }
 
--- packages/web/src/pages/Home/styles.js
+++ packages/web/src/pages/Home/styles.js
@@ -1,17 +1,8 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
-
-export const Container = styled.div`
-  margin: 0px auto;
-  max-width: 1300px;
-  padding: 8px calc(16px);
-  width: 100%;
-`;
-
 export const Content = styled.div`
   display: grid;
-  grid-gap: 3.2rem;
+  grid-gap: 2.4rem;
   grid-template-columns: repeat(3, 1fr);
   padding: 1.6rem 0;
 `;
