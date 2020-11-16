90abee1c0f7eb97abc1477071a1eeeaf7e68a615
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-11-16 01:02:33 +0000

    feat: navbar (#12)
    * :rocket: add navbar component
    
    * :rocket: add home page

---
+++ packages/web/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from 'components/Navbar';
+
+import * as S from './styles';
+
+function Home() {
+  return (
+    <S.Wrapper>
+      <Navbar />
+    </S.Wrapper>
+  );
+}
+
+export default Home;
