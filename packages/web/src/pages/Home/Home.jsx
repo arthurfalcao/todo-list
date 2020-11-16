9670d8fb5f9529c9d440a9d1cee44d9de7524b0c
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-11-16 01:41:34 +0000

    :rocket: add CreateProjectForm to the home page

--- packages/web/src/pages/Home/Home.jsx
+++ packages/web/src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import CreateProjectForm from 'components/CreateProjectForm';
 import Navbar from 'components/Navbar';
 
 import * as S from './styles';
@@ -6,6 +7,12 @@ function Home() {
   return (
     <S.Wrapper>
       <Navbar />
+
+      <S.Container>
+        <S.Content>
+          <CreateProjectForm />
+        </S.Content>
+      </S.Container>
     </S.Wrapper>
   );
 }
--- packages/web/src/pages/Home/styles.js
+++ packages/web/src/pages/Home/styles.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div``;
+
+export const Container = styled.div`
+  margin: 0px auto;
+  max-width: 1300px;
+  padding: 8px calc(16px);
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  display: grid;
+  grid-gap: 3.2rem;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 1.6rem 0;
+`;
